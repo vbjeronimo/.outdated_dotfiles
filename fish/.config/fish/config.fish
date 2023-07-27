d5583214342865060fea848d1477f9abe9cf3838
Vitor Jeronimo <vitor.bently@hotmail.com>
2023-07-27 06:48:36 -0300

    Added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+set fish_greeting
+set TERM "xterm-256color"
+set EDITOR "nvim"
+
+set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+
+alias l="exa -l --icons --group-directories-first"
+alias ll="exa -la --icons --group-directories-first --git"
+alias t="exa -T --icons"
+alias tt="exa -Ta --icons"
+
+starship init fish | source
