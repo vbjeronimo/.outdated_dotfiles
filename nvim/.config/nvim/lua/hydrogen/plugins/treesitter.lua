bc686a44b1a144449b8f760e599ce15f0bcac2b4
Vitor Jeronimo <vitor.bently@hotmail.com>
2023-07-31 07:43:22 -0300

    Fixed typo in treesitter config

--- nvim/.config/nvim/lua/hydrogen/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/hydrogen/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-objects",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
